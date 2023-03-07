1217cfa24276bcc8a6b43b9a9424f084d9437912
shraja7 <shraja7@gmail.com>
2023-03-06 21:12:23 -0600

    anagram algo

---
+++ anagram/anagram.js
@@ -0,0 +1,26 @@
+const anagram = (str1, str2) => {
+  const aCharMap = buildCharMap(str1);
+  const bCharMap = buildCharMap(str2);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+};
+
+//buildCharMap
+const buildCharMap = (str) => {
+  const charMap = {};
+
+  for (let char of str.toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+};
+
+module.exports = anagram;
